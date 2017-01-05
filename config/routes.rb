a17d9cf62b33843d6245797112c8d40357e073d3
feizheng <1290657123@qq.com>
2017-01-05 13:34:27 +0800

    add new resours

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'applicants/new'
+  get 'applicants/create'
+
+  post 'sessions/create'
+  post 'applicants/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
