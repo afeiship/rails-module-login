e0606f50c760aa52dd227a1768747a0a33ae3915
afeiship <1290657123@qq.com>
2017-01-18 22:17:22 +0800

    intial

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'applicants/new'
-  get 'applicants/create'
-
-  post 'sessions/create'
-  post 'applicants/create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
