65d0d229161ce2443754a9df911ff5f7ea4bac47
afeiship <1290657123@qq.com>
2017-03-26 22:54:29 +0800

    register & login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'login'=>'users#login', :as=>'login'
+  get 'register'=>'users#new'
+  post 'create'=>'users#create'
   post 'create_login_session' => 'users#create_login_session'
+  
 
+  root :to => 'pages#welcome' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
